4ea4a5967ca1f57661824fdc89d850e92dea2485
Octavian Popa <octavian.popa@windeln.ro>
2016-01-22 10:25:39 +0200

    Example of spring data query

--- src/main/java/com/popa/books/repository/BookRepository.java
+++ src/main/java/com/popa/books/repository/BookRepository.java
@@ -4,8 +4,11 @@ import com.popa.books.model.Book;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository  extends CrudRepository<Book, Long>{
 
+    List<Book> findByTitle(String title);
 
 }
