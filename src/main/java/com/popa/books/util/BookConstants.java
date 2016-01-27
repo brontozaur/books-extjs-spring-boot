0b024f083c14222b63d6574f2b923721e0d6b5a2
Octavian Popa <octavian.popa@windeln.ro>
2016-01-27 17:12:46 +0200

    Experiments with tests

--- src/main/java/com/popa/books/util/BookConstants.java
+++ src/main/java/com/popa/books/util/BookConstants.java
@@ -7,5 +7,6 @@ public class BookConstants {
 
     public static final String PROFILE_DEV = "dev";
     public static final String PROFILE_TEST = "test";
+    public static final String PROFILE_INT = "int";
 
 }
